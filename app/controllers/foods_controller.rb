72d5632ae457f534e93954a975eac13a10f99d54
skylible <sourcewidi@gmail.com>
2020-03-14 20:57:09 +0700

    Fixed indentation in food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -28,7 +28,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to @food, notice: 'Food was successfully created.' }
+        format.html { redirect_to @food, notice: 'Food created successfully.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new }
@@ -56,19 +56,20 @@ class FoodsController < ApplicationController
   def destroy
     @food.destroy
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
+      format.html { redirect_to foods_url, notice: 'Food destroy success.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_food
-      @food = Food.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def food_params
-      params.require(:food).permit(:name, :description, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_food
+    @food = Food.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def food_params
+    params.require(:food).permit(:name, :description, :price)
+  end
 end

56d83e42fb651c37566227ce67ff06fb73550938
skylible <sourcewidi@gmail.com>
2020-03-14 21:11:17 +0700

    Added no attributes given case for Food model test

--- spec/model/food_spec.rb
+++ spec/model/food_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
+  it 'is not valid without any attributes' do
+    expect(Food.new(nil)).to_not be_valid
+  end
+
   it 'is valid with valid attributes' do
-    expect(Food.new(name: :nasigoreng, description: :goreng, price: 200.0)).to be_valid
+    args = {
+      name: :nasigoreng, 
+      description: :goreng, 
+      price: 200.0
+    }
+    expect(Food.new(args)).to be_valid
   end
 end
